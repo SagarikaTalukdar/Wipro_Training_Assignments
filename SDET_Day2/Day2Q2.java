5ab00887a8b70f00146b93ae8f11e725fc57b20b
SagarikaTalukdar <sagarikatalukdar13@gmail.com>
2026-05-07 21:52:35 +0530

    Add files via upload

---
+++ SDET_Day2/Day2Q2.java
@@ -0,0 +1,21 @@
+package SDET_Day2;
+
+//Login Validation
+
+public class Day2Q2 {
+	public static void main(String[] args) {
+		
+		String cusername = "Sagarika";
+		String cpassword = "1234";
+		
+		String username = "Sagarika";
+		String password = "1234";
+		
+		if (username == cusername && password == cpassword) {
+			System.out.println("Login Succesful!");
+		}
+		else {
+			System.out.println("Login Failure!");
+		}
+	}
+}
