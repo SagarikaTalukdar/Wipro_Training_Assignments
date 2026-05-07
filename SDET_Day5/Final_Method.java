41b337fa05adc99c1966655c0a1eeba8c9f6f710
SagarikaTalukdar <sagarikatalukdar13@gmail.com>
2026-05-07 22:35:02 +0530

    Add files via upload

---
+++ SDET_Day5/Final_Method.java
@@ -0,0 +1,26 @@
+package SDET_Day5;
+
+/* 2. You are building a payment system where the method processPayment() contains secure logic and should not be 
+	overridden by any subclass. Question: How will you design a class using the final keyword to prevent method 
+	overriding? Implement with an example.
+*/
+
+class PaymentSystem{
+    final void processPayment() {
+        System.out.println("Processing secure payment...");
+    }
+}
+
+class Ccard extends PaymentSystem {
+    void show() {
+        System.out.println("Payment done using Credit Card");
+    }
+}
+
+public class Final_Method {
+    public static void main(String[] args) {
+        Ccard obj = new Ccard();
+        obj.processPayment();
+        obj.show();
+    }
+}
